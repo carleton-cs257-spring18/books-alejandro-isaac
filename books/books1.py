4e2c584cd52d1221fa8f227cab04778dd392bf46
gallardoa <gallardoa@carelton.edu>
2019-04-06 18:39:06 -0500

    Can print sorted list forward or backward given sort direction or not for authors or books

--- books/books1.py
+++ books/books1.py
@@ -8,21 +8,32 @@ except IndexError:
     exit(1)
 
 try:
-    action = sys.argv[2]
+    action = sys.argv[2].lower()
 except IndexError:
     print("Error! Please provide an action.")
     exit(1)
-    
+
+try:
+    sort_direction = sys.argv[3].lower()
+    if sort_direction == "forward":
+        sortReverse= False
+    else:
+        sortReverse = True
+except IndexError:
+    sortReverse = False
+
 with open(csv_file, newline = '') as csvfile:
     reader = csv.reader(csvfile)
     target_list = []
-    if action.lower() == "books":
+    if action == "books":
         item = 0
-    elif action.lower() == "authors":
+    elif action == "authors":
         item = 2
         
     for line in reader:
         target_list.append(line[item])
+        
+    print(sorted(target_list, reverse = sortReverse))
 
-    print(target_list)
+
         
