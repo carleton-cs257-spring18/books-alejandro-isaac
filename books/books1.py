a99b41b1b7bb0e60e1aac0a3753cefd78de9a97e
gallardoa <gallardoa@carelton.edu>
2019-04-07 15:55:38 -0500

    prints value one per output line

--- books/books1.py
+++ books/books1.py
@@ -4,13 +4,15 @@ import csv
 try:
     csv_file = sys.argv[1]
 except IndexError:
-    print("Error! Please provide the filename of the csv file.")
+    print('Usage: python3 books1.py input-file action [sort-direction]', file=sys.stderr)
+    print('At the very least, please include .csv file and an action!')
     exit(1)
 
 try:
     action = sys.argv[2].lower()
 except IndexError:
-    print("Error! Please provide an action.")
+    print('Usage: python3 books1.py input-file action [sort-direction]', file=sys.stderr)
+    print('At the very least, please include .csv file and an action!')
     exit(1)
 
 try:
@@ -22,18 +24,53 @@ try:
 except IndexError:
     sortReverse = False
 
-with open(csv_file, newline = '') as csvfile:
-    reader = csv.reader(csvfile)
-    target_list = []
-    if action == "books":
-        item = 0
-    elif action == "authors":
-        item = 2
+def createList():
+    with open(csv_file, newline = '') as csvfile:
+        reader = csv.reader(csvfile)
+        target_list = []
+        if action == "books":
+            item = 0
+        elif action == "authors":
+            item = 2
+            
+        for line in reader:
+            if action == "authors":
+                author = line[item].split("(")[0]
+                author_lastName = author.split(' ')[-2]
+                if [author_lastName,author] not in target_list:
+                    target_list.append([author_lastName,author])
+            else:
+                target_list.append(line[item])
+        return sorted(target_list, reverse = sortReverse)
+
+def printList(target_list):
+    for item in target_list:
+        if action == "books":
+            print(item)
+        elif action == "authors":
+            print(item[1])
+
+def main():
+    target_list = createList()
+    printList(target_list)
+
+main()
         
-    for line in reader:
-        target_list.append(line[item])
+#method below uses dictionaries as data structure instead of lists. 
+#much simpler. 
+# with open(csv_file, newline = '') as csvfile:
+#     reader = csv.reader(csvfile)
+#     target_dict = {}
         
-    print(sorted(target_list, reverse = sortReverse))
+#     for line in reader:
+#         author = line[2].split("(")[0]
+#         book_title = line[0]
+#         target_dict[author] = [book_title]
+#     if action == "books":
+#         target_list = list(target_dict.values())
+#     elif action == "authors":
+#         target_list = list(target_dict.keys())
+#     print(sorted(target_list, reverse = sortReverse))
 
 
         
