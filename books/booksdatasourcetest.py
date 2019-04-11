610f2b28632dd8475d2e79ac8bc14d188d307c9b
Isaac Reynaldo <reynaldoi@carleton.edu>
2019-04-11 17:15:46 -0500

    added phase 2 files

---
+++ books/booksdatasourcetest.py
@@ -0,0 +1,17 @@
+'''
+    booksdatasourcetest.py
+    Alejandro Gallardo & Isaac Reynaldo, 11 April 2019
+'''
+
+import booksdatasource
+import unittest
+
+class BooksDataSourceTest(unittest.TestCase):
+    def set_up(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
